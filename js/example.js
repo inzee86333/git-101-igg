a9aa2093f6617f77cb5a94f997b601e9835f6e12
inzee86333 <56289715+inzee86333@users.noreply.github.com>
2020-09-16 13:49:27 +0700

    quiz1

--- js/example.js
+++ js/example.js
@@ -1,6 +1,10 @@
 let color = {
     rad: "rad",
-    blue: "blue"
+    blue: "blue",
+    black: {
+        black1: "black1",
+        black2: "black2" 
+    }
 }
 let arr = [
     "1ตัว",
@@ -11,4 +15,4 @@ let arr = [
 ]
 const pi = 3.14;
 namePi = 'Pi';
-console.log(`${namePi}:`, pi, color.blue, arr[color.blue.length]);
+console.log(`${namePi}:`, pi, color.black.black1, arr[color.blue.length]);
---
+++ js/quiz1.js
@@ -0,0 +1,8 @@
+let firstName = "Supamit";
+let lastName = "Padtip";
+let nickName = "Inzee";
+let age = 21;
+let hopbies = "Playing Computer Games"
+let phoneNumber = "0948610872"
+
+console.log(`Fistname: ${firstName} \nLastname: ${lastName}\nNickname: ${nickName}\nAge: ${age}\nHopbies: ${hopbies}\nPhone: ${phoneNumber}`)
