f2aa65f3f048aa00a5333990a7e2a180d388d3e9
inzee86333 <56289715+inzee86333@users.noreply.github.com>
2020-09-16 13:39:19 +0700

    ่js_example

---
+++ js/example.js
@@ -0,0 +1,14 @@
+let color = {
+    rad: "rad",
+    blue: "blue"
+}
+let arr = [
+    "1ตัว",
+    "2ตัว",
+    "3ตัว",
+    "4ตัว",
+    "5ตัว"
+]
+const pi = 3.14;
+namePi = 'Pi';
+console.log(`${namePi}:`, pi, color.blue, arr[color.blue.length]);
