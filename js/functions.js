6c224c2152ec7b4df7e33b72c5e69116c511ccb9
inzee86333 <56289715+inzee86333@users.noreply.github.com>
2020-09-16 14:04:35 +0700

    ArrowFunction

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,15 @@ function mul(a, b){
 }
 
 console.log(mul(2, 5))
+
+function searchCustommer(custommerName){
+    return{
+        name: "Custommer Name"
+    }
+}
+
+const addArrow = (a, b) => {
+    return a+b
+}
+
+console.log(addArrow(2, 5))
